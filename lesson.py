8ef83b23f3b6dacac9106bf1103d44673c4f7636
DanteDisward <nikitachernigin@gmail.com>
2024-08-07 00:20:39 +0500

    V0.1

---
+++ lesson.py
@@ -0,0 +1,16 @@
+#1st program
+print(9 ** 0.5 * 5)
+
+#2st program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3st program
+task1 = 2*2+2
+task2 = 2*(2+2)
+print(task1)
+print(task2)
+print(task1 > task2)
+
+#4st program
+date = int(float("123.456")*10)%10
+print(date)
